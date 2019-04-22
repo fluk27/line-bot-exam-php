a554cfafd5d6c110eb6fc3e481a2bf0439280c2e
fluk27 <40637925+fluk27@users.noreply.github.com>
2019-04-22 14:28:37 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,10 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'xgSfHeINj8YpcpM4KuWCu367JHMYru2cxC9kxY9iXQG';
+$access_token = 'etm3cPn4oxw/p0hkrsbYx2/rc3G1UuhFWcS37hGymegbT52hHJYlpO2ovFEp7IvsvXSYQqTbLF6MOGyH32ch9HYzZ/XnG4HBgdHHleklq2oWBYWhfuBIT8BNmAJEVKSxMJf2IG8a5Xtnw03w0aLsQAdB04t89/1O/w1cDnyilFU=
+';
 
-$channelSecret = '+2ZQJoMcXfR38L+IQZDFR58776PYOOOa2tldpbsixa5EtU0piBoYY4bIXnFEbDA6vXSYQqTbLF6MOGyH32ch9HYzZ/XnG4HBgdHHleklq2prFBDEccNDhqfFb6cDNZN+/GjLglgK4o956d1YdQVMUQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'fd2831d734e43f8c2a2c150e0dea1336';
 
 $pushID = 'U6e6a50f10c56bd642a22a4d91259b9f0';
 
